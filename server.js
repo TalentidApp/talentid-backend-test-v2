5c9c0de1af5e6ae57fe9e16486e758347305d430
chavarahul <chavarahul7@gmail.com>
2025-04-05 18:37:22 +0530

    solved issue

--- server.js
+++ server.js
@@ -39,21 +39,7 @@ app.use(bodyParser.json({ limit: "50mb" }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-const allowedOrigins = [
-  "http://localhost:3000",
-  "https://offers.talentid.app",
-  "http://localhost:5173"
-];
-
-app.use((req, res, next) => {
-  const origin = req.headers.origin;
-  if (allowedOrigins.includes(origin)) {
-    res.setHeader("Access-Control-Allow-Origin", origin);
-  }
-  res.setHeader("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
-  next();
-});
+app.use(cors({ origin: ["http://localhost:3000/",'https://offers.talentid.app/','http://localhost:5173/'], credentials: true }));
 app.set('trust proxy', 1);
 // Define __dirname manually
 const __filename = fileURLToPath(import.meta.url);
